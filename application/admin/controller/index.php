0a77cd3f24b8abd190a7eb8d6baf4ab9ef9e4efb
www <mnmnwq@qq.com>
2019-11-11 16:56:51 +0800

    aa

---
+++ application/admin/controller/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baiwei
+ * Date: 2019/11/11
+ * Time: 16:53
+ */
+namespace app\admin\controller;
+use think\Controller;
+
+class index extends Controller {
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function main()
+    {
+        return view('main');
+    }
+
+    /**
+     * 头部
+     * @return \think\response\View
+     */
+    public function head()
+    {
+        return view('head');
+    }
+
+    /**
+     * 左导航
+     */
+    public function left()
+    {
+        return view('left');
+    }
+}
