3bb2da1b283597ee3ee1186fe3c941736bc7d646
www <mnmnwq@qq.com>
2019-11-13 16:12:57 +0800

    aa

---
+++ application/admin/controller/Brand.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baiwei
+ * Date: 2019/11/13
+ * Time: 16:06
+ */
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Brand extends  Controller{
+    public function add()
+    {
+        
+    }
+}
