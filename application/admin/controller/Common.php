c09ead10e2ab0998a490348a782001098625841c
www <mnmnwq@qq.com>
2019-11-20 14:31:46 +0800

    aa

--- application/admin/controller/Common.php
+++ application/admin/controller/Common.php
@@ -4,6 +4,7 @@ use think\Controller;
 
 class Common extends Controller{
 	public function __construct(){
+		parent::__construct();
         //登陆鉴权
 //        if(!session('admin_id')){
 //            $this->error('请登录','Login/index');
