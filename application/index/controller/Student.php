7e040a6ec84abc829e73e6c5934926be8b09e8a8
www <mnmnwq@qq.com>
2019-11-22 14:30:57 +0800

    aa

--- application/index/controller/Student.php
+++ application/index/controller/Student.php
@@ -14,4 +14,29 @@ class Student extends Controller{
 		
 		return view('index',['info'=>$info]);
 	}
+
+	public function add(){
+		return view('add');
+	}
+	public function do_add(){
+		$post = input();
+		if($_FILES['image']['error'] != 0){
+			$this->error('错误1111111111');
+		}
+		//正常
+		//tp5的文件上传操作
+		// 获取表单上传文件 例如上传了001.jpg 
+		$file = request()->file('image');
+		// 移动到框架应用根目录/uploads/ 目录下 
+		$info = $file->move( './uploads');
+		if($info){
+			//成功
+			// 成功上传后 获取上传信息
+			$image_info = $info->getSaveName();
+		}else{
+			//失败
+			$this->error('错误');
+		}
+		echo '/uploads/'.$image_info;
+	}
 }
