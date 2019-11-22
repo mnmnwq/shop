8b7556f5624318cc41b9d8159dc6f7999ce2fe80
www <mnmnwq@qq.com>
2019-11-22 12:45:26 +0800

    aa

--- application/index/controller/Student.php
+++ application/index/controller/Student.php
@@ -5,7 +5,13 @@ use think\Controller;
 use app\model\StudentData;
 class Student extends Controller{
 	public function index(){
-		$info = StudentData::select();
+		$info = StudentData::paginate(2);
+		//中间页
+		//区分调用哪个模板
+		if(request()->isAjax()){
+			return view('ajax_page',['info'=>$info]);
+		}
+		
 		return view('index',['info'=>$info]);
 	}
 }
