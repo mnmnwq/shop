8a221f470eb496e350dcfca6564eaa43f4f0f4ee
www <mnmnwq@qq.com>
2019-12-03 10:33:56 +0800

    aa

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -7,11 +7,33 @@
  */
 namespace app\index\controller;
 
+use app\model\ShopUser;
 use think\Controller;
 
 class Login extends Controller{
     public function index()
     {
         return view('index');
+    }
+
+    /**
+     * 登陆操作
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function do_login()
+    {
+        $username = input('username','');
+        $userpass = input('userpass','');
+        $user_info= ShopUser::where(['username'=>$username,'user_pwd'=>md5($userpass)])->find();
+        if($user_info){
+            //登陆成功，写入session
+            session('user_id',$user_info['id']);
+            $this->success('登陆成功！','Index/index');
+        }else{
+            //登陆失败
+            $this->error('登陆失败！');
+        }
     }
 }
---
+++ application/model/ShopUser.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baiwei
+ * Date: 2019/12/3
+ * Time: 10:29
+ */
+namespace app\model;
+use think\Model;
+class ShopUser extends Model{
+    protected $pk = 'id';
+    protected $table = 'shop_user';
+}
