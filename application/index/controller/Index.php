8f71f27573e00e71d06a3e6f1e3821f1d7849162
www <mnmnwq@qq.com>
2019-12-03 14:38:20 +0800

    aa

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,12 +2,35 @@
 namespace app\index\controller;
 
 
+use app\model\ShopCate;
+
 class Index
 {
     public function index()
     {
-        return view('index');
+        $cate = ShopCate::select()->toArray();
+        $cate_info = $this->createSonTree($cate);
+        return view('index',['cate_info'=>$cate_info]);
     }
+
+    /**
+     *
+     */
+    public function createSonTree($data,$pid=0)
+    {
+        $arr = [];
+        if(!$data){
+            return [];
+        }
+        foreach($data as $k=>$v){
+            if($pid == $v['pid']){
+                $arr[$k] = $v;
+                $arr[$k]['son'] = $this->createSonTree($data,$v['id']);
+            }
+        }
+        return $arr;
+    }
+
 
     public function hello($name = 'ThinkPHP5')
     {
