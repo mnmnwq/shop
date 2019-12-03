cde8f9b33992918220b51829e24e4528906688b1
www <mnmnwq@qq.com>
2019-12-03 10:26:34 +0800

    aa

---
+++ application/index/controller/Category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baiwei
+ * Date: 2019/12/3
+ * Time: 9:32
+ */
+namespace app\index\controller;
+use think\Controller;
+class Category extends Controller{
+    public function index()
+    {
+        return view('index');
+    }
+}
