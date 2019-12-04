db5e6cac5f6b3013418b620e47bc4132d5565aca
www <mnmnwq@qq.com>
2019-12-04 14:39:20 +0800

    aa

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -6,15 +6,26 @@
  * Time: 9:32
  */
 namespace app\index\controller;
+use app\model\ShopBrand;
 use app\model\ShopCate;
+use app\model\ShopGoods;
 use think\Controller;
 class Category extends Controller{
     public function index()
     {
         $cate_id = input('cate_id','0');
+        $brand_id = input('brand_id','0');
+        $where = [];
+        if($cate_id){
+            $where[] = ['cate_id','=',$cate_id];
+        }
+        if($brand_id){
+            $where[] = ['brand_id','=',$brand_id];
+        }
         $pid = ShopCate::where(['id'=>$cate_id])->value('pid');
         $cate_list = ShopCate::where(['pid'=>$pid])->select();
-        $goods_info = ShopGoods::where(['cate_id'=>$cate_id])->paginate(20,false,['query'=>input()]);
-        return view('index',['cate_list'=>$cate_list]);
+        $brand_info = ShopBrand::select();
+        $goods_info = ShopGoods::where($where)->paginate(20,false,['query'=>input()]);
+        return view('index',['cate_list'=>$cate_list,'brand_info'=>$brand_info,'goods_info'=>$goods_info]);
     }
 }
