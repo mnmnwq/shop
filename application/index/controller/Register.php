c1c4dd009a464bfd5c0403fbbd9d4b7e139c5225
www <mnmnwq@qq.com>
2019-12-04 09:33:34 +0800

    aa

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -6,6 +6,7 @@
  * Time: 9:55
  */
 namespace app\index\controller;
+use app\model\ShopUser;
 use think\Controller;
 
 class Register extends Controller{
@@ -14,8 +15,31 @@ class Register extends Controller{
         return view('index');
     }
 
+    /**
+     * 执行注册
+     */
     public function do_register()
     {
         //执行注册
+        $username = input('user_name','');
+        $userpass = input('user_pass','');
+        $re_pass = input('re_pass','');
+        $email = input('email','');
+        $tel = input('tel','');
+        if($userpass != $re_pass){
+            $this->error('密码不一致！');
+        }
+        $shop_user = new ShopUser();
+        $shop_user->username = $username;
+        $shop_user->user_pwd = md5($userpass);
+        $shop_user->user_tel = $tel;
+        $shop_user->user_email = $email;
+        $shop_user->user_time = time();
+        $result = $shop_user->save();
+        if($result){
+            $this->success('成功！','Index/index');
+        }else{
+            $this->error('失败！');
+        }
     }
 }
